d5da0d25ba4d7ee4081894d4aedfc20c3daf1fa8
Prath T <prathep@outlook.com>
2022-04-08 12:20:27 -0400

    Added Keyboard Input
    Able to play instruments through keyboard entry.

--- The Complete 2022 Web Development Bootcamp/13. Drum Kit/index.js
+++ The Complete 2022 Web Development Bootcamp/13. Drum Kit/index.js
@@ -13,57 +13,65 @@ function hangleClick(){
 */
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
-    document.querySelectorAll("button")[i].addEventListener("click", function(){
-    
-
-
-            //this.setAttribute('style', 'color: blue');
-            this.style.color = "blue"
-
-            var buttonInnerHTML = this.innerHTML;
-
-            switch (buttonInnerHTML){
-                case "w":
-                    var tom1 = new Audio('sounds/tom-1.mp3');
-                    tom1.play();
-                break;
-
-                case "a":
-                    var tom2 = new Audio('sounds/tom-2.mp3');
-                    tom2.play();
-                break;
-
-                case "s":
-                    var tom3 = new Audio('sounds/tom-3.mp3');
-                    tom3.play();
-                break;
-
-                case "d":
-                    var tom4 = new Audio('sounds/tom-4.mp3');
-                    tom4.play();
-                break;
-
-                case "j":
-                    var snare = new Audio('sounds/snare.mp3');
-                    snare.play();
-                break;
-
-                case "k":
-                    var crash = new Audio('sounds/crash.mp3');
-                    crash.play();
-                break;
-
-                case "l":
-                    var kickBass = new Audio('sounds/kick-bass.mp3');
-                    kickBass.play();
-                break;
-
-                default: consol.log(buttonInnerHTML);
-            }
-        
-        }
-
-    )
+    document.querySelectorAll("button")[i].addEventListener("click", function(e){
+        console.log(e.target.innerHTML);
+        keyExecute(e.target.innerHTML);
+        });
 }
 
 
+document.addEventListener("keypress", function(e){
+console.log(e.key);
+keyExecute(e.key)
+});
+
+
+function keyExecute(key){
+    
+
+
+    //this.setAttribute('style', 'color: blue');
+    //this.style.color = "blue"
+
+
+
+    switch (key){
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+        break;
+
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+        break;
+
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+        break;
+
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+        break;
+
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+        break;
+
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+        break;
+
+        case "l":
+            var kickBass = new Audio('sounds/kick-bass.mp3');
+            kickBass.play();
+        break;
+
+        default: console.log(buttonInnerHTML);
+    }
+
+}
