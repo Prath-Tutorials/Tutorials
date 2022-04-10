26151583bd3a3aff97e85a49e7f8ed7942380083
Prath T <prathep@outlook.com>
2022-04-10 14:16:33 -0400

    203 Step 8 - Check the User's Answer Against the Game Sequence8

--- The Complete 2022 Web Development Bootcamp/14. Simon Game Challenge/game.js
+++ The Complete 2022 Web Development Bootcamp/14. Simon Game Challenge/game.js
@@ -1,13 +1,13 @@
 let buttonColours = ["red", "blue", "green", "yellow"]
 let gamePattern = [];
+let userClickedPattern  = [];
 var soundBlue = new Audio("sounds/blue.mp3");
 var soundGreen = new Audio("sounds/green.mp3");
 var soundRed = new Audio("sounds/red.mp3");
 var soundYellow = new Audio("sounds/yellow.mp3");
 var soundWrong = new Audio("sounds/wrong.mp3");
 
-var userChosenColour = "";
-let userClickedPattern  = [];
+
 
 var level = 0;
 
@@ -29,22 +29,23 @@ function playColorSound(whatColor){
 
 
 function nextSequence(){
+    userClickedPattern  = []
     randomNumber = Math.floor(Math.random()*4); // generate a new random number between 0 and 3
-    console.log(randomNumber);
+    //console.log(randomNumber);
 
     randomColor = buttonColours[randomNumber];
-    console.log(randomColor);
+    //console.log(randomColor);
 
     playColorSound(randomColor);
     $("#"+randomColor).fadeOut(100).fadeIn(100);
     
     gamePattern.push(randomColor);
-    console.log(gamePattern);
+    //console.log("gamePattern | "+gamePattern);
 
     level++;
     $('h1')[0].innerText = "Level " + level;
     
-    console.log(level);
+    //console.log(level);
 
     //return randomColor;
     
@@ -55,12 +56,55 @@ document.addEventListener("keydown", keyDownTextField, false);
 
 function keyDownTextField(e) {
   var keyCode = e.key;
-  console.log(keyCode);
-  if ($('h1')[0].innerText == "Press A Key to Start") {
+  //console.log(keyCode);
+  if ($('h1')[0].innerText == "Press A Key to Start" || $('h1')[0].innerText == "Game Over, Press Any Key to Restart") {
       $('h1')[0].innerText = "Level 0";
-      nextSequence();
+      startOver();
+      setTimeout(function () {
+          startOver();
+        nextSequence();
+       
+    }, 1000);
+      
     }
   
+
+}
+
+
+
+function checkAnswer(currentLevel){
+    //console.log("userClickedPattern | "+userClickedPattern[currentLevel]);
+    //console.log("gamePattern | "+gamePattern[currentLevel]);
+
+
+    //console.log("userClickedPattern.length | "+userClickedPattern.length);
+    //console.log("gamePattern.length | "+gamePattern.length);
+
+    
+        if(userClickedPattern[currentLevel] == gamePattern[currentLevel]){
+
+            if (userClickedPattern.length === gamePattern.length){
+                
+                setTimeout(function () {
+                    nextSequence();
+                   
+                }, 1000);
+            }
+
+            //console.log("success");
+        } else{
+            soundWrong.play()
+            $("body").addClass("game-over");
+            $("#level-title").text("Game Over, Press Any Key to Restart");
+      
+            setTimeout(function () {
+              $("body").removeClass("game-over");
+            }, 200);
+      
+            startOver();
+        }
+     
 
 }
 
@@ -68,17 +112,42 @@ function keyDownTextField(e) {
 $('[type="button"]').click(function(){
     var myID = this.id;
 
-    userChosenColour = myID;
-    console.log(userChosenColour);
-    userClickedPattern.push(myID);
-    console.log(userClickedPattern);
-    playColorSound(myID);
+    if ($('h1')[0].innerText == "Press A Key to Start" || $('h1')[0].innerText == "Game Over, Press Any Key to Restart")  {
+        // Game has not started
+        
+    } else {
+        
+        userClickedPattern.push(myID);
+        //console.log("userClickedPattern | "+userClickedPattern);
+        playColorSound(myID);
 
 
-    $("#"+myID).addClass('pressed');
+        $("#"+myID).addClass('pressed');
 
-    setTimeout(function() {
-        $("#"+myID).removeClass('pressed');
-    }, 100);
+        setTimeout(function() {
+            $("#"+myID).removeClass('pressed');
+        }, 100);
+
+        //console.log("myID | "+myID);
+        //console.log("userClickedPattern[0] | "+userClickedPattern[0]);
+    
+    
+        //console.log("level" + level);
+        
+    
+        checkAnswer(userClickedPattern.length-1);
+
+    }
+
+
+
 
   });
+
+
+  function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+  }
+  
