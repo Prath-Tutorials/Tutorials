3d452f7b4eb43e1aab4fb56360b8729140f67a32
Prath T <prathep@outlook.com>
2022-04-12 10:48:24 -0400

    Retrieving data from POST command
    This code retries data and logs the value to the terminal

--- The Complete 2022 Web Development Bootcamp/19. Express.js with Node.js/Calculator/calculator.js
+++ The Complete 2022 Web Development Bootcamp/19. Express.js with Node.js/Calculator/calculator.js
@@ -1,6 +1,8 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true})); //Video - 235 Processing Post Requests with Body Parser
 
 app.get("/", function(req, res){
     //res.send("Hello");
@@ -8,9 +10,12 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
+    console.log(req.body); // Logs the value that get returned
+    console.log(req.body.num1); // Logs the value num1 that get returned
+    console.log(req.body.num2); // Logs the value num2 that get returned
     res.send("Thanks for posting that");
 });
 
 app.listen(3000, function(){
-    console.log("Rnning");
+    console.log("Running");
 });
