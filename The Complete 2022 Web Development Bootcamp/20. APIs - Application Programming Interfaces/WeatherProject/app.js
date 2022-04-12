b2c0374d7a526acb5d028ddef6f9093ffcd48834
Prath T <prathep@outlook.com>
2022-04-12 19:53:31 -0400

    244 How to Parse JSON
    This is pulling the data form the API and parsing the JSON

--- The Complete 2022 Web Development Bootcamp/20. APIs - Application Programming Interfaces/WeatherProject/app.js
+++ The Complete 2022 Web Development Bootcamp/20. APIs - Application Programming Interfaces/WeatherProject/app.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const https = require("https"); // A native Node.ja module
+
+
 
 const app = express();
 
 app.get("/", function(req, res){
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Markham&appid=2c5b6ba77c18bb9fc702aaac3839885d&units=metric";
+    https.get(url, function(response){
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherDataInHexidecimal = data;
+            const weatherDataInJSON = JSON.parse(data);
+            console.log(weatherDataInJSON);
+
+            const weatherDataInJSONStingify = JSON.stringify(weatherDataInJSON);
+            console.log(weatherDataInJSONStingify);
+
+            const weatherValue = weatherDataInJSON.main.temp;
+            console.log(weatherValue);
+
+            const weatherDescription = weatherDataInJSON.weather[0].description;
+            console.log(weatherDescription);
+
+            
+        });
+    });
+
     res.send("Server is up and running");
 });
 
