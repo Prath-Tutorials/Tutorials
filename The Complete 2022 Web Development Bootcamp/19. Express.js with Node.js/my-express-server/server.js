0b54e850c1bd95a3af33cd802932735f6adaf7c1
Prath T <prathep@outlook.com>
2022-04-12 09:06:43 -0400

    Added Routes
    Added 3 Routes in total

--- The Complete 2022 Web Development Bootcamp/19. Express.js with Node.js/my-express-server/server.js
+++ The Complete 2022 Web Development Bootcamp/19. Express.js with Node.js/my-express-server/server.js
@@ -1,11 +1,21 @@
-//228 Handling Requests and Responses_ the GET Request
 
 const express = require('express');
 const app = express();
 
-app.get("/", function(request , response){ //  "/" is the root directory
-  console.log(request); //  this will log all the information on that request
-  response.send("<h1>Hello World</h1>");
+app.get("/", function(req , res){ //  "/" is the root directory
+  console.log(req); //  this will log all the information on that request
+  res.send("<h1>Hello World</h1>");
+})
+
+
+app.get("/contact", function(req , res){ //  "/" is the root directory
+  console.log(req); //  this will log all the information on that request
+  res.send("<h1>Contact me at: 555-555-5555</h1>");
+})
+
+app.get("/aboutme", function(req , res){ //  "/" is the root directory
+  console.log(req); //  this will log all the information on that request
+  res.send("<h1>Hi, I'm Prathep. I'm a programmer.");
 })
 
 app.listen(3000, function() {
