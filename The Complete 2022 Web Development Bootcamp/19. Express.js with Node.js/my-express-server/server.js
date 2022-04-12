3f4858f5fd5d352f417ac1cb9e071b4b7135457d
Prath T <prathep@outlook.com>
2022-04-12 08:31:05 -0400

    228 Handling Requests and Responses_ the GET Request
    Added response

--- The Complete 2022 Web Development Bootcamp/19. Express.js with Node.js/my-express-server/server.js
+++ The Complete 2022 Web Development Bootcamp/19. Express.js with Node.js/my-express-server/server.js
@@ -1,8 +1,11 @@
+//228 Handling Requests and Responses_ the GET Request
+
 const express = require('express');
 const app = express();
 
-app.get("/", function(request , response){
-  console.log(request);
+app.get("/", function(request , response){ //  "/" is the root directory
+  console.log(request); //  this will log all the information on that request
+  response.send("<h1>Hello World</h1>");
 })
 
 app.listen(3000, function() {
