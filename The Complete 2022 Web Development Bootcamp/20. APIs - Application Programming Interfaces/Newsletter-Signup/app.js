70aea5cd631a62bde3b436341efb287c59609e94
Prath T <prathep@outlook.com>
2022-04-13 10:19:03 -0400

    Site is able to retrieve the css and images
    app.use(express.static("public"));
    
    Adding this to the app.js we are able to access files in the 'public' folder

--- The Complete 2022 Web Development Bootcamp/20. APIs - Application Programming Interfaces/Newsletter-Signup/app.js
+++ The Complete 2022 Web Development Bootcamp/20. APIs - Application Programming Interfaces/Newsletter-Signup/app.js
@@ -2,15 +2,16 @@ const express = require("express");           //npm i express
 const bodyParser = require("body-parser");    //npm i body-parser
 const request = require("request");           //npm i request
 
-const app = express();                        // new instance of Express
+const app = express();   // new instance of Express
 
+app.use(express.static("public"));  // This is needed for the website to access files like images and css. 'public' is the name of the folder
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
 
-
-
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html");
 });
+
+
