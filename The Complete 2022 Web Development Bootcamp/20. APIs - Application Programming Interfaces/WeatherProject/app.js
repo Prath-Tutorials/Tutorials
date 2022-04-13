a2b9e0079a2a2e9bcf635d2ad7d3df6328088b6d
Prath T <prathep@outlook.com>
2022-04-12 22:50:20 -0400

    245 Using Express to Render a Website with Live API Data
    Gets and displays weather data using the openweathermap API

--- The Complete 2022 Web Development Bootcamp/20. APIs - Application Programming Interfaces/WeatherProject/app.js
+++ The Complete 2022 Web Development Bootcamp/20. APIs - Application Programming Interfaces/WeatherProject/app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.get("/", function(req, res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Markham&appid=2c5b6ba77c18bb9fc702aaac3839885d&units=metric";
+    
     https.get(url, function(response){
         console.log(response.statusCode);
 
@@ -24,11 +25,19 @@ app.get("/", function(req, res){
             const weatherDescription = weatherDataInJSON.weather[0].description;
             console.log(weatherDescription);
 
-            
+            const weatherIcon = weatherDataInJSON.weather[0].icon;
+            console.log(weatherIcon);
+
+            //res.send("<h1> The temperature in London is " + weatherValue + " degrees Celcius. </h1>\n The weather desciption is: " + weatherDescription);
+
+            res.write("<h1> The temperature in London is " + weatherValue + " degrees Celcius. </h1>");
+            res.write("The weather desciption is: " + weatherDescription);
+            res.write("The weather icon is: <img src=http://openweathermap.org/img/wn/" + weatherIcon+".png\>");
+            res.send()
         });
     });
 
-    res.send("Server is up and running");
+    //res.send("Server is up and running"); You could only have once 'send' per block
 });
 
 
