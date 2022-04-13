919556ece29a3a9ff383f32b3666f23b02e57f43
Prath T <prathep@outlook.com>
2022-04-13 14:05:56 -0400

    Fully functional Newsletter signup site

--- The Complete 2022 Web Development Bootcamp/20. APIs - Application Programming Interfaces/Newsletter-Signup/app.js
+++ The Complete 2022 Web Development Bootcamp/20. APIs - Application Programming Interfaces/Newsletter-Signup/app.js
@@ -50,6 +50,13 @@ app.post("/", function(req, res){
     }
 
     const request = https.request(url, options, function(response){
+
+        if(response.statusCode == 200){
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+
         response.on("data", function(data){
             console.log(JSON.parse(data));
         })
@@ -58,6 +65,11 @@ app.post("/", function(req, res){
     request.write(jsonData);
     request.end();
 
+});
+
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
 });
 
 //API KEY - 79e6fdd9d58caad20028752752cd0acb-us14
