ec622c882d948cd8a9d97244cc94c0d968ac66a9
Prath T <prathep@outlook.com>
2022-04-16 19:12:04 -0400

    Show Homepage + content
    Using app.js display home and the content of homeStartingContent

--- The Complete 2022 Web Development Bootcamp/23. Boss Level Challenge 3 - Blog Website/app.js
+++ The Complete 2022 Web Development Bootcamp/23. Boss Level Challenge 3 - Blog Website/app.js
@@ -1,4 +1,4 @@
-//jshint esversion:6
+
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -17,7 +17,12 @@ app.use(express.static("public"));
 
 
 
+app.get("/", function(req, res){
+  res.render("home",{
+    listTitle: homeStartingContent,
 
+})
+});
 
 
 
