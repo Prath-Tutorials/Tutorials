53d54523a50603f33a07f6220d01c9a469d6d779
Prath T <prathep@outlook.com>
2022-04-16 17:24:07 -0400

    Added another module to the date.js
    Added getDay() and we are able to access it date.getDate()

--- The Complete 2022 Web Development Bootcamp/22. EJS/ToDoList-V1/app.js
+++ The Complete 2022 Web Development Bootcamp/22. EJS/ToDoList-V1/app.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
-    let day = date(); // this is calling the function in date.js
+    let day = date.getDate(); // this is calling the function in date.js [date.getDate() or date.getDay()]
     res.render("list", { 
         listTitle: day,
         newListItems: newItems
--- The Complete 2022 Web Development Bootcamp/22. EJS/ToDoList-V1/date.js
+++ The Complete 2022 Web Development Bootcamp/22. EJS/ToDoList-V1/date.js
@@ -1,6 +1,8 @@
 
 
-module.exports = getDate; //there is no bracket after getDate since we don't want to call it when exporting
+//module.exports = getDate; //there is no bracket after getDate since we don't want to call it when exporting
+module.exports.getDate = getDate; //there is no bracket after getDate since we don't want to call it when exporting
+
 
 function getDate(){
 
@@ -10,6 +12,21 @@ function getDate(){
         weekday: "long",
         day: "numeric",
         month: "long"
+    }
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    return day;
+}
+
+module.exports.getDay = getDay; //there is no bracket after getDate since we don't want to call it when exporting
+
+function getDay(){
+
+
+    let today = new Date();
+    let options = {
+        weekday: "long",
     }
 
     let day = today.toLocaleDateString("en-US", options);
