0cd250f6470f00081957940e72d3f46d8a87d600
Prath T <prathep@outlook.com>
2022-04-27 17:29:10 -0400

    Retrieve data from external JSON file
    Logs the values to the console after

--- Complete React Developer in 2022 (Redux, Hooks, GraphQL)/1. Monsters Rolodex/monster-rolodex/src/App.js
+++ Complete React Developer in 2022 (Redux, Hooks, GraphQL)/1. Monsters Rolodex/monster-rolodex/src/App.js
@@ -6,28 +6,39 @@ class App extends Component {
   constructor() {
     super(); // This calls the constructor method of Component
 
+    /*
+        this.state = {
+          monster: [
+            { id: "f9rwer", name: 'Linda' },
+            { id: "Itr5e5", name: 'Frank' },
+            { id: "8Or458", name: 'Jacky' }
+          ]
+        };
+    */
 
     this.state = {
-      monster: [
-        { id: "f9rwer", name: 'Linda' },
-        { id: "Itr5e5", name: 'Frank' },
-        { id: "8Or458", name: 'Jacky' }
-      ]
+      monster: []
     };
   }
-
-    render(){
-      return (
-        <div className="App">
-          {
-            this.state.monster.map((monster) => {
-              return <h1 key= {monster.id}> {monster.name} </h1>
-            })
-          }
-        </div>
-      );
-    }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((respond) => respond.json())
+      .then((users) => console.log(users))
   }
+
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monster.map((monster) => {
+            return <h1 key={monster.id}> {monster.name} </h1> // key must to be added to the parent tag only
+          })
+        }
+      </div>
+    );
+  }
+}
 
 
 export default App;
