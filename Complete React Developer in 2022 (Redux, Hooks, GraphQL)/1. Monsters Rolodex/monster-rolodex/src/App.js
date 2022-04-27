a9ed4f604afd4dff17cdf32f377d8b8736b84404
Prath T <prathep@outlook.com>
2022-04-27 09:18:21 -0400

    Changing greeting message via button click

--- Complete React Developer in 2022 (Redux, Hooks, GraphQL)/1. Monsters Rolodex/monster-rolodex/src/App.js
+++ Complete React Developer in 2022 (Redux, Hooks, GraphQL)/1. Monsters Rolodex/monster-rolodex/src/App.js
@@ -3,6 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super(); // This calls the constructor method of Component
+
+    this.state = {
+      name: 'Prathep'
+    }
+  }
 
   render() {
     return (
@@ -10,16 +17,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi {this.state.name}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={() => {
+            this.setState({name: 'Thurairatnam'})
+          }}>
+            Change Name
+          </button>
+            
+          
         </header>
       </div>
     );
