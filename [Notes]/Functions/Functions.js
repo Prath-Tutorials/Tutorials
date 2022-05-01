498a2f9c57f0afab91aecedea242e0c929854297
Prath T <prathep@outlook.com>
2022-05-01 14:38:03 -0400

    Added couple of notes

---
+++ [Notes]/Functions/Functions.js
@@ -0,0 +1,19 @@
+// Simple JS Function
+
+function add1(a,b) {
+    return a + b;
+}
+add1(5, 10);
+
+
+// Arrow Functions
+const add2 = (a, b) => a + b
+add2(5, 10);
+
+
+// Arrow Functions with Return
+const add3 = (a, b) => {
+    return a + b;
+}
+add3(5, 10);
+
---
+++ [Notes]/Objects/Promise.js
@@ -0,0 +1,3 @@
+// Promise
+// The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
