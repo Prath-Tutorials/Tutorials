d57c410ff70047e51329e41c34e1513b265ccc6b
Prath T <prathep@outlook.com>
2022-05-02 09:39:20 -0400

    Gets a random quote

--- [Boilerplate ]/HTML + CSS + JS/assets/js/script.js
+++ [Boilerplate ]/HTML + CSS + JS/assets/js/script.js
@@ -1,11 +1,37 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const quoteAuthor = document.getElementById('author')
+const quoteTwitter = document.getElementById('twitter')
+const newQuoteButton = document.getElementById('new-quote')
+
 let apiQuotes = [];
+
 
 // Get a single quote
 function newQupte(){
     // Pick a random quotes form apiQuotes array
 
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    
+
+    // Check if Author feild is blank
+    if(!quote.author){
+        quoteAuthor.textContent="Unknown";
+    } else {
+        quoteAuthor.textContent=quote.author;
+    }
+
+    // Check Quote Length to determine styling
+    if (quote.text.length > 100){
+        quoteText.classList.add('long-quote')
+    } else
+    {
+        quoteText.classList.remove('long-quote')
+    }
+
+    quoteText.textContent=quote.text;
+
+    console.log(quote.author);
 }
 
 
